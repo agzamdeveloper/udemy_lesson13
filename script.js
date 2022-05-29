fcc8c0b11f56497d6f4fc2834a06f3e15d5952bf
Agzam Asadullaev <agzamasadullaev@gmail.com>
2022-05-29 12:08:35 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//alert("Hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("How old are you?", "18");
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt("What is your name?", "");
+answers[1] = prompt("What is your surname?", "");
+answers[2] = prompt("How old are you?", "");
+
+// document.write(answers);
+
+console.log(typeof(answers));
